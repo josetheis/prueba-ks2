42210e7b42e5bec54e2b792f3e17b2c5a7c46e2f
Jose Theis <main.josesanchez@gmail.com>
2025-04-28 18:53:24 -0400

    agregando tailwind y las primeras interfaces

---
+++ src/interfaces/AddUserDTO.ts
@@ -0,0 +1,4 @@
+export default interface AddUserDTO {
+    name: string;
+    email: string;
+}
---
+++ src/interfaces/User.ts
@@ -0,0 +1,8 @@
+export default interface User {
+    id: number;
+    name: string;
+    email: string;
+    status: 'ACTIVE' | 'INACTIVE';
+    createdAt: string;
+    updatedAt: string;
+}
